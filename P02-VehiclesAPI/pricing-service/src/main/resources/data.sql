717d17e7b93df011980c4708157f6258228cd353
Krzysztof Pagacz <Krzysztof.Pagacz1@Vodafone.com>
2022-08-19 09:56:49 +0200

    setting up eureka-server

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(501, 'USD', 3000.0);
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(502, 'USD', 4000.5);
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(503, 'USD', 3500);
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(504, 'USD', 3700);
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(505, 'USD', 6000);
+INSERT INTO PRICE(vehicleId, currency, price) VALUES(506, 'USD', 9200);
