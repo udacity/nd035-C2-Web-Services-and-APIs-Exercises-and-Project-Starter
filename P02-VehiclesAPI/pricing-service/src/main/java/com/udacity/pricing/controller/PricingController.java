bba628423236427fff71f47cd7e26475a201b732
Krzysztof Pagacz <Krzysztof.Pagacz1@Vodafone.com>
2022-09-12 13:10:22 +0200

    restore some pricing service classes

---
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/controller/PricingController.java
@@ -0,0 +1,25 @@
+package com.udacity.pricing.controller;
+
+/**
+ * Implements a REST-based controller for the pricing service.
+ */
+@RestController
+@RequestMapping("/services/price")
+public class PricingController {
+
+    /**
+     * Gets the price for a requested vehicle.
+     * @param vehicleId ID number of the vehicle for which the price is requested
+     * @return price of the vehicle, or error that it was not found.
+     */
+    @GetMapping
+    public Price get(@RequestParam Long vehicleId) {
+        try {
+            return PricingService.getPrice(vehicleId);
+        } catch (PriceException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Price Not Found", ex);
+        }
+
+    }
+}
