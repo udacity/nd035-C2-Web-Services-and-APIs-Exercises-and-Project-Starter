ee197798837b3a40e2dfb47d708c5593deac3ed2
Haneen Alnuwaysir <haneenalnuwaysir@gmail.com>
2024-05-18 22:38:23 +0300

    error with price service

--- P02-VehiclesAPI/eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ P02-VehiclesAPI/eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class EurekaApplication {
 	}
 
 }
+
--- P02-VehiclesAPI/eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ P02-VehiclesAPI/eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.eureka;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class EurekaApplicationTests {
+public class EurekaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
