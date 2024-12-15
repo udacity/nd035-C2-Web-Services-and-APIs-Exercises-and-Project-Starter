09d66f50ffe55876472fd69f21c3db588a1085bc
Neha Verma <nehbehl@gmail.com>
2024-12-15 11:13:52 +0530

    Update BootstrapApplicationTests.java

--- Lesson2-restapis/bootstrap/src/test/java/com/udacity/bootstrap/BootstrapApplicationTests.java
+++ Lesson2-restapis/bootstrap/src/test/java/com/udacity/bootstrap/BootstrapApplicationTests.java
@@ -1,15 +1,11 @@
 package com.udacity.bootstrap;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BootstrapApplicationTests {
 
-	@Test
+	@org.junit.jupiter.api.Test
 	public void contextLoads() {
 	}
 
