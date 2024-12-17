976d44992e91d1a0bb0d29eb71b5f803e8e124a6
Neha Verma <nehbehl@gmail.com>
2024-12-17 20:56:52 +0530

    Update Location.java

--- Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/entity/Location.java
+++ Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/entity/Location.java
@@ -1,6 +1,9 @@
 package com.udacity.graphql.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Location {
