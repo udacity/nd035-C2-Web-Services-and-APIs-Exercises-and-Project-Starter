299b8ad66b32ca8d6815ef9199b99ff1d6b34f22
Neha Verma <nehbehl@gmail.com>
2024-12-17 20:58:07 +0530

    Update Query.java

--- Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/resolver/Query.java
+++ Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/resolver/Query.java
@@ -1,18 +1,21 @@
 package com.udacity.graphql.resolver;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.udacity.graphql.entity.Location;
 import com.udacity.graphql.repository.LocationRepository;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Query implements GraphQLQueryResolver {
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Query{
     private LocationRepository locationRepository;
 
     public Query(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
     }
 
+    @QueryMapping
     public Iterable<Location> findAllLocations() {
         return locationRepository.findAll();
     }
