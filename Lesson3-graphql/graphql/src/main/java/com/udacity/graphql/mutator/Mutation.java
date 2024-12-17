7f39bdcd1eee9af54faa743fc7396a8dbc6bb417
Neha Verma <nehbehl@gmail.com>
2024-12-17 20:57:31 +0530

    Update Mutation.java

--- Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/mutator/Mutation.java
+++ Lesson3-graphql/graphql/src/main/java/com/udacity/graphql/mutator/Mutation.java
@@ -1,33 +1,39 @@
 package com.udacity.graphql.mutator;
 
-import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.udacity.graphql.entity.Location;
 import com.udacity.graphql.exception.LocationNotFoundException;
 import com.udacity.graphql.repository.LocationRepository;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.util.Optional;
 
-@Component
-public class Mutation implements GraphQLMutationResolver {
+@Controller
+public class Mutation{
     private LocationRepository locationRepository;
 
     public Mutation(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
     }
 
-    public Location newLocation(String name, String address) {
+    @MutationMapping
+    public Location newLocation(@Argument String name,@Argument String address) {
         Location location = new Location(name, address);
         locationRepository.save(location);
         return location;
     }
 
-    public boolean deleteLocation(Long id) {
+    @MutationMapping
+    public boolean deleteLocation(@Argument Long id) {
         locationRepository.deleteById(id);
         return true;
     }
 
-    public Location updateLocationName(String newName, Long id) {
+    @MutationMapping
+    public Location updateLocationName(@Argument String newName,@Argument Long id) {
         Optional<Location> optionalLocation =
                 locationRepository.findById(id);
 
