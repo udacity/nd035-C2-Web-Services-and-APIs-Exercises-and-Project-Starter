b5fd7abc0ccb0d53cfdcb9532764ad206de63e34
Neha Verma <nehbehl@gmail.com>
2024-12-21 16:14:51 +0530

    Update MicroservicesApplication.java

--- Lesson4-microservices/items/src/main/java/com/udacity/microservices/MicroservicesApplication.java
+++ Lesson4-microservices/items/src/main/java/com/udacity/microservices/MicroservicesApplication.java
@@ -3,10 +3,9 @@ package com.udacity.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+//@EnableDiscoveryClient
 public class MicroservicesApplication {
 
     public static void main(String[] args) {
