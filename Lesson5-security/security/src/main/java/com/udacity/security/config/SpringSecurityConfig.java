6334a79aea3929a1388fab0ae7d462f71341bed1
Neha Verma <nehbehl@gmail.com>
2024-12-23 18:02:06 +0530

    Update SpringSecurityConfig.java

--- Lesson5-security/security/src/main/java/com/udacity/security/config/SpringSecurityConfig.java
+++ Lesson5-security/security/src/main/java/com/udacity/security/config/SpringSecurityConfig.java
@@ -1,42 +1,29 @@
 package com.udacity.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password(encoder().encode("password"))
-                .roles("USER");
-    }
-
+public class SpringSecurityConfig {
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .formLogin(httpForm ->{
+                    httpForm.defaultSuccessUrl("/locations");
+                    
+                })            
+                .authorizeHttpRequests(registry ->{
+                    registry.anyRequest().authenticated();
+                })              
+
+                .build();       
     }
 }
 
