f16a9766c1dc99ff47bee00f10e5d7db12262518
Neha Verma <nehbehl@gmail.com>
2024-12-27 16:56:14 +0530

    Update LocationControllerUnitTest.java

--- Lesson8-testing/src/test/java/com/udacity/test/web/LocationControllerUnitTest.java
+++ Lesson8-testing/src/test/java/com/udacity/test/web/LocationControllerUnitTest.java
@@ -1,22 +1,19 @@
 package com.udacity.test.web;
 
-import com.udacity.test.service.LocationService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+import com.udacity.test.service.LocationService;
+
 @WebMvcTest(LocationController.class)
 public class LocationControllerUnitTest {
     @Autowired
@@ -27,9 +24,9 @@ public class LocationControllerUnitTest {
 
     @Test
     public void getAllLocations() throws Exception {
-        mockMvc.perform(get("/location/"))
+        mockMvc.perform(get("/location"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[]"));
 
         verify(locationService, times(1)).retrieveLocations();
